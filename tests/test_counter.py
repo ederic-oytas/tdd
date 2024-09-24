0e2925958491e5df51c191283dd79481f4c8d6c1
Ederic Oytas <edericoytas@gmail.com>
2024-09-24 14:41:09 -0700

    Add updating and reading; add settings.json

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,3 +10,58 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+import pytest
+
+# we need to import the unit under test - counter
+from src.counter import app 
+
+# we need to import the file that contains the status codes
+from src import status
+
+
+@pytest.fixture()
+def client():
+    return app.test_client()
+
+@pytest.mark.usefixtures("client")
+class TestCounterEndPoints:
+    """Test cases for Counter-related endpoints"""
+
+    def test_create_a_counter(self, client):
+        """It should create a counter"""
+        result = client.post('/counters/foo')
+        assert result.status_code == status.HTTP_201_CREATED
+
+    def test_duplicate_a_counter(self, client):
+        """It should return an error for duplicates"""
+        result = client.post('/counters/bar')
+        assert result.status_code == status.HTTP_201_CREATED
+        result = client.post('/counters/bar')
+        assert result.status_code == status.HTTP_409_CONFLICT
+
+    def test_update_a_counter(self, client):
+        result = client.post('/counters/fiz')
+        assert result.status_code == status.HTTP_201_CREATED
+        assert result.json['fiz'] == 0
+        result = client.put('/counters/fiz')
+        assert result.status_code == status.HTTP_200_OK
+        assert result.json['fiz'] == 1
+
+    def test_update_not_found(self, client):
+        result = client.put('/counters/buz')
+        assert result.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_read_a_counter(self, client):
+        result = client.post('/counters/raz')
+        assert result.status_code == status.HTTP_201_CREATED
+        assert result.json['raz'] == 0
+        result = client.put('/counters/raz')
+        assert result.status_code == status.HTTP_200_OK
+        assert result.json['raz'] == 1
+        result = client.get('/counters/raz')
+        assert result.status_code == status.HTTP_200_OK
+        assert result.json['raz'] == 1
+
+    def test_read_not_found(self, client):
+        result = client.get('/counters/jaz')
+        assert result.status_code == status.HTTP_404_NOT_FOUND
